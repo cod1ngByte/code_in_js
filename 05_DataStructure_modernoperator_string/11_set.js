5b3c49e3113d803d03a1c4959e3e23014d11a63b
cod1ngByte <rajiv.ranjansoc@gmail.com>
2023-12-24 13:41:03 +0530

    set file added

--- 05_DataStructure_modernoperator_string/11_set.js
+++ 05_DataStructure_modernoperator_string/11_set.js
@@ -8,9 +8,60 @@
             setname.add(value)               |                 arrayname.push(element), arrayname.unshift(element)
             setname.delete(value)            |                 arrayname.pop(), arrayname.shift()
             setname.has(value)               |                 arrayname.includes(element)
-            setname.size()                   |                 arrayname.length()
-
+            setname.size                     |                 arrayname.length
+ 
 */
+//we can have any iterable inside set
 
-const orderSet = ["pasta", "pizza", "pizza", "risetto", "pasta", "Pizza"];
-console.log(orderSet);
+const orderSet = new Set([
+    "pasta",
+    "pizza",
+    "pizza",
+    "risetto",
+    "pasta",
+    "Pizza",
+]);
+console.log(orderSet); // pizza and Pizza both are two diff value.
+//  { 'pasta', 'pizza', 'risetto', 'Pizza' }
+
+//string are also iterable.
+console.log(new Set("missipi")); // {'m','i','s','p'}
+
+// in set
+
+console.log(orderSet.size);
+console.log(orderSet.has("Bread")); //false
+console.log(orderSet.has("pasta")); //true
+console.log(orderSet.add("garlic bread")); //Set(5) { 'pasta', 'pizza', 'risetto', 'Pizza', 'garlic bread' }
+console.log(orderSet.delete("resetto")); //false
+console.log(orderSet.delete("risetto")); //true
+
+// delete all elements from set using clear()
+
+// console.log(orderSet.clear());
+// console.log(orderSet);
+
+// for-of loop can be used in all iterable and set is also iterable.
+
+for (const order of orderSet) {
+    console.log(order);
+}
+
+//------------------------------->remove duplicate from array using set
+
+const staff = ["waiter", "chef", "waiter", "manager", "chef", "waiter"];
+const uniqueStaff = new Set(staff); // pass array ref or array itslef
+console.log(uniqueStaff);
+
+//--------------------------------> convert set into an array using spread op
+// const staffUniqueArr = [...new Set([staff])] create set then use spread to convert in an array
+const staffUniqueArr = [...uniqueStaff];
+console.log(staffUniqueArr);
+
+//--------------------------------->checking unique element in array //pass to set then use size prop
+
+console.log(new Set([1, 2, 4, 2, 1, 5, 2, 1]).size);
+
+//--------------------------------> number of different character in string
+
+console.log(new Set("missipii").size);
