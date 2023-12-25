718b700e1469847b3e642457b88d88311ca16c9e
cod1ngByte <rajiv.ranjansoc@gmail.com>
2023-12-25 18:37:07 +0530

    function file updated

--- 02_Basic/01_function.js
+++ 02_Basic/01_function.js
@@ -9,16 +9,16 @@
 --> return keyword in function automatically exit the function.
 */
 function sayMyName() {
-  console.log("c");
-  console.log("o");
-  console.log("d");
-  console.log("1");
-  console.log("n");
-  console.log("g");
-  console.log("B");
-  console.log("y");
-  console.log("t");
-  console.log("e");
+    console.log("c");
+    console.log("o");
+    console.log("d");
+    console.log("1");
+    console.log("n");
+    console.log("g");
+    console.log("B");
+    console.log("y");
+    console.log("t");
+    console.log("e");
 }
 
 sayMyName(); // function calling (function block is executed).
@@ -26,10 +26,10 @@ sayMyName(); // function calling (function block is executed).
 // console.log(sayMyName); function reference is passed so it will print functin defination , it will not execute the function
 
 function addTwoNumber(num1, num2) {
-  //num1 and num2 is parameter
-  //we don't need to defined parameter using let/const/var keyword
-  //num1 and num2 will be defined once addTwoNumber() is called.
-  console.log(num1 + num2);
+    //num1 and num2 is parameter
+    //we don't need to defined parameter using let/const/var keyword
+    //num1 and num2 will be defined once addTwoNumber() is called.
+    console.log(num1 + num2);
 }
 addTwoNumber(); // both parameter will store undefined and Number(undefiend) is NaN, so NaN
 addTwoNumber(2, 3); // 5
@@ -39,8 +39,8 @@ const result = addTwoNumber(2 + 3); //function will print NaN and since funtion 
 console.log(result); // undefined
 
 function sum(num1, num2) {
-  return num1 + num2;
-  //console.log('hello'); --> unreachable code since return is used before this line.
+    return num1 + num2;
+    //console.log('hello'); --> unreachable code since return is used before this line.
 }
 
 const sumReturned = sum(2, 5); //-->return the 2+5 stored in sumReturned variable.
@@ -48,43 +48,43 @@ console.log(sumReturned); //--> 7
 
 //--> if we don't pass argument the function paramert will stored undefined value.
 function loginMsg(username) {
-  if (!username) {
-    console.log("please mention argument will calling loginMsg function");
-  } else {
-    console.log(`Welcome ${username} `);
-  }
+    if (!username) {
+        console.log("please mention argument will calling loginMsg function");
+    } else {
+        console.log(`Welcome ${username} `);
+    }
 }
 loginMsg();
 loginMsg("cod1ngByte");
 
 // -->passing object in function.
 const gameContra = {
-  username: "drie",
-  score: 45,
+    username: "drie",
+    score: 45,
 };
 function handleObject(obj) {
-  console.log(
-    `username is ${obj.username} and score is ${obj.score} in Contra game`
-  );
+    console.log(
+        `username is ${obj.username} and score is ${obj.score} in Contra game`
+    );
 }
 handleObject(gameContra);
 
 // passing array in function
 
 function firstElementInArray(arr) {
-  console.log(arr[0]);
+    console.log(arr[0]);
 }
 firstElementInArray([10, 20]); // we can even pass array reference like object reference in above example.
 
 // using rest parameter in function (used in cart item to add)
 
 function calculateCartPrice(...price) {
-  //mulitple value in single array
-  console.log(price); // it will print array.
+    //mulitple value in single array
+    console.log(price); // it will print array.
 }
 calculateCartPrice(10, 20, 30);
 
 /*
-console.log() is a function in javascirpt which prints the information in console.
+console.log() is a method in javascirpt which prints the information in console.
 
 */
