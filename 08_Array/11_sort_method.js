b888cb9dea93ce629e23ac6a933159727ea5a6ef
cod1ngByte <rajiv.ranjansoc@gmail.com>
2024-01-04 16:06:19 +0530

    sort method

---
+++ 08_Array/11_sort_method.js
@@ -0,0 +1,34 @@
+//sort() method mutate the original array
+//sort() method sort based on the string by defualt (convert the element into string and doest the sorting)
+
+const owners = ["rajiv", "cod1ng", "Adam", "Martha"];
+
+console.log(owners.sort());
+console.log(owners); //mutate the original array
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// console.log(movements.sort()); // sort the array a/c to string and mutate the original array
+
+//----------ascending order sorting
+
+// movements.sort((currEle, nextEle) => {
+//     if (currEle > nextEle) return 1;
+//     if (currEle < nextEle) return -1;
+// });
+
+//or
+
+movements.sort((a, b) => a - b);
+console.log(movements);
+
+//----------descending order sorting
+
+// movements.sort((curEle, nextElem) => {
+//     if (curEle > nextElem) return -1;
+//     if (curEle < nextElem) return 1;
+// });
+
+//or
+movements.sort((a, b) => b - a);
+console.log(movements);
