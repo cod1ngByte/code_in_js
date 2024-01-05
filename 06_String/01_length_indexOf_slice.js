5b39ee567fdf11c407b1511281840a670d37710d
cod1ngByte <rajiv.ranjansoc@gmail.com>
2024-01-05 18:48:50 +0530

    updated string

--- 06_String/01_length_indexOf_slice.js
+++ 06_String/01_length_indexOf_slice.js
@@ -1,6 +1,7 @@
 /*
-     String is primitive type so any changes is string will not effect the original string,
-     i.e string is immutable, each string method applied, result will be stored in new string, no changes is original string
+     String is primitive type so any changes in string will not effect the original string,
+     i.e string is immutable,so if any string method applied,
+     result will be stored in new string, no changes is original string
      
 */
 
@@ -12,6 +13,7 @@ const plane = "A320";
 console.log(plane[0]); //A
 console.log(airline[2]); // P
 console.log(plane[2]); //2
+console.log(plane[8]); // if index in given to access character is greater than length-1 it will return undefined.
 console.log("javascript"[4]); //s
 
 //zero based like array index also zero based
@@ -21,30 +23,36 @@ console.log("javascript"[4]); //s
 console.log(airline.length); // 16 (no of character in airline string literal )
 console.log("javascript".length); //10
 
-//------------------------------------------------->method : indexOf()
+//------------------------------------------------->method : indexOf()-------------------------------
 /*
-//return the index of first occurance,
-// if not found return -1
+string.indexOf(searchString,position)
+//position is optional
+//return the first occurrence index or return -1
+//position > length -1 , return -1
+//position < 0 , search from the index zero ie. search entire string
+//searchString will be searched from the position mentioned in 2nd arg
+//case sensitive
+// return position when searching empty string.
 
-//indexOf(character)
-//indexOf(character,fromIndex)  : fromIndex is optional
-
-               formIndex  :
-                    // 1. if fromIndex is not present, search from zero index
-                    // 2. if fromIndex < str.length , search from front to back from index mentioned in fromIndex
-                    // 3. if fromIndex >= str.length, return -1
-                    // 4. if formIndex is -ve, counts back from the string literal but search from front to back only
-                    // 5. if fromIndex < -str.length, return -1
-                     
-// same for lastIndexOf() but it will return the last occurence.
+'hello world hello'.indexOf('o', -5) returns 4 -- ( search from index zero since p < 0) 
+'hello world hello'.indexOf('world', 12) returns -1 -- ( search from index 12, )
+'hello world hello'.indexOf('o', 99) returns -1 -- ( p > lenght -1 so return -1)
+"hello world".indexOf(""); // returns 0
+"hello world".indexOf("", 0); // returns 0
+"hello world".indexOf("", 3); // returns 3
+"hello world".indexOf("", 99); // returns 99
 
 
-     eg : const array = [2, 9, 9];
-          array.indexOf(2); // 0
-          array.indexOf(7); // -1
-          array.indexOf(9, 2); // 2
-          array.indexOf(2, -1); // -1
-          array.indexOf(2, -3); // 0
+
+//-------------------------------------------lastIndexOf(searchString,position)---------------------------
+//position is optional
+//return index or -1
+//if p > length -1 , search from index 0
+//if p < 0 , search at index zero or return -1
+//searchString will search till the position mentioned in 2nd argument
+//case senstive
+//for empty string.. 'canal'.lastIndexOf("") //return 5(which is length)
+                    //'canal'.lastIndexOf("",3) //return 3(which is 2nd arg)
 
 */
 
@@ -53,7 +61,7 @@ console.log(airline.lastIndexOf("r")); //10
 console.log(airline.indexOf("portugal")); // -1 (case sensitive)
 console.log(airline.indexOf("Portugal")); //8
 
-//------------------------------------------------------->method slice()
+//------------------------------------------------------->method slice()-------------------------------------
 /*
 
 slice() method takes index as argument and indexOf() returns index so we  can use indexOf() in slice()
@@ -61,18 +69,20 @@ slice() method takes index as argument and indexOf() returns index so we  can us
 //slice() : everything in string is extracted if used
 //slice(startIndex) 
 //slice(startIndex,endIndex) : startIndex is included but endIndex is not included
+//slice() method returns the substring or else return empty string
+
 
      startIndex : (optional)
           //1. statIndex < str.length :- normal 
           //2. if startIndex  is -ve :- count from back but extract is from front to back
-          //3 . if startIndex < -str.length :- extract from zero index
-          //4. if startIndex >= str.length :- nothing is extracted
+          //3 . if startIndex < -str.length and startIndex < 0 :- extract from zero index
+          //4. if startIndex >= str.length :- nothing is extracted, return empty string
 
      endIndex : (optional)  | not included 
           //1. endindex < str.length : normal 
           //2. endIndex is -ve :- count from back but extract is from front to back
           //3. endIndex < -str.length :- extract from zero is used
-          //4. endIndex >= str.length :- stirng length is used, extracting all elements until the end 
+          //4. endIndex >= str.length :- treat endIndex as zero 
 
 */
 console.log(airline.slice(4)); //Air Portugal
