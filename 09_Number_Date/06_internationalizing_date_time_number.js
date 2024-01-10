973db86205aa150f9ce354c7e462de123c316869
cod1ngByte <rajiv.ranjansoc@gmail.com>
2024-01-10 15:23:36 +0530

    number format

--- 09_Number_Date/06_internationalizing_date_time_number.js
+++ 09_Number_Date/06_internationalizing_date_time_number.js
@@ -30,3 +30,25 @@ const options = {
 // const locale = navigator.language;
 
 console.log(new Intl.DateTimeFormat("en-IN", options).format(now)); //Wednesday, 10/1/2024, 14:31
+
+//---------------------------------Number Format-----------------------------------------------------------
+
+const num = 3884764.23;
+
+console.log(new Intl.NumberFormat("en-IN").format(num)); //38,84,764.23
+console.log(new Intl.NumberFormat("en-US").format(num)); //3,884,764.23
+
+const options1 = {
+    style: "currency", // 'unit' || 'percent
+    unit: "celsius",
+    currency: "EUR",
+    // useGrouping: false,
+};
+
+console.log("US:      ", new Intl.NumberFormat("en-US", options).format(num));
+console.log("Germany: ", new Intl.NumberFormat("de-DE", options).format(num));
+console.log("Syria:   ", new Intl.NumberFormat("ar-SY", options).format(num));
+// console.log(
+//   navigator.language,
+//   new Intl.NumberFormat(navigator.language, options).format(num)
+// );
